659bf30a8051e388eea559ef1aed49e037d2636e
casimirMorency <morencycasimir@gmail.com>
2022-08-26 17:54:42 -0400

    Refactoring codes

--- src/component/registration-patient/patient-form/patient-registration/input/custom-input/idenfiersInput/id-utils.ts
+++
@@ -1,9 +0,0 @@
-export function formatNif(number) {
-    return number?.replace(/\D/g, "").match(/.{1,3}/g)?.join("-").substr(0, 13) || ""
-
-}
-
-export function formatCin(number) {
-    return number?.replace(/\D/g, "").match(/.{1,3}/g)?.join("").substr(0, 10) || ""
-
-}
--- src/component/registration-patient/patient-form/patient-registration/input/custom-input/phone/normlizePhoneNumber.ts
+++
@@ -1,5 +0,0 @@
-
-export default function formatPhoneNumber(phoneNumberString) {
-    return phoneNumberString.replace(/\D/g, "").match(/.{1,4}/g)?.join("-").substr(0, 9) || ""
-}
-
